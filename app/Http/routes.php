c7e2a77027d696159f016c31d5c94d3f176542e2
FHW <fuhanwen03@163.com>
2017-08-15 23:16:54 +0800

    test2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,3 +32,62 @@ FORM;
 Route::post('/hello',function(){
     return "Hello Laravel[POST]!";
 });
+#参数
+Route::get('/hello/{name}',function($name){
+    return "Hello {$name}!";
+});
+#多个参数
+Route::get('/hello/{name}/by/{user}',function($name,$user){
+    return "Hello {$name} by {$user}!";
+});
+#注意以上参数是必选的，如果没有输入参数会抛出MethodNotAllowedHttpException或NotFoundHttpException异常。
+
+#此外闭包函数中的参数与路由参数一一对应。
+Route::get('/hello/{name}/{user}',function($name,$user){
+    return "Hello {$name} by {$user}!";
+});
+
+Route::get('/hello1/{name?}',function($name="Laravel"){
+    return "Hello {$name}!";
+});
+
+#取别名
+/*Route::get('/hello/laravelacademy',['as'=>'academy',function(){
+    return 'Hello LaravelAcademy！';
+}]);*/
+
+Route::get('/hello/laravelacademy1/{id}',['as'=>'academy',function($id){
+    return 'Hello LaravelAcademy2 '.$id.'！';
+}]);
+
+Route::get('/testNamedRoute',function(){
+    return redirect()->route('academy');
+});
+
+Route::get('/testNamedRoute1',function(){
+    return redirect()->route('academy',['id'=>2]);
+});
+
+Route::get('/testNamedRoute2',function(){
+    return redirect()->route('admin::dashboard2');
+});
+
+Route::group(['as' => 'admin::'], function () {
+    Route::get('dashboard3', ['as' => 'dashboard2', function () {
+        return "Hello hahhah!";
+    }]);
+});
+
+
+Route::group(['middleware'=>'test'],function(){
+    Route::get('/write/laravelacademy',function(){
+        //使用Test中间件
+    });
+    Route::get('/update/laravelacademy',function(){
+        //使用Test中间件
+    });
+});
+
+Route::get('/age/refuse',['as'=>'refuse',function(){
+    return "未成年人禁止入内！";
+}]);
