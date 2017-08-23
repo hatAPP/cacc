e4883baf6ad69dcd906410e10f75f8e328a21559
傅晗文 <fuhanwen03@163.com>
2017-08-23 12:12:38 +0800

    sehelve change test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,9 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/-', function () {
-    return view('welcome');
-});
 Route::get('/hello',function(){
     return "Hello Laravel[GET]!";
 });
