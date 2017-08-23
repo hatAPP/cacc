674b17466d4f9a8b8423415081c35d3a74cfb9c0
hatAPP <fuhanwen03@163.com>
2017-08-23 11:20:41 +0800

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/-', function () {
+    return view('welcome');
+});
 Route::get('/hello',function(){
     return "Hello Laravel[GET]!";
 });
